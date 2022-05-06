6253c3df003bb3a48ccd22ac7f2d774ed400c0cb
AnthonyUPTN <tagirovantn@gmail.com>
2022-05-06 17:40:43 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './sass/main.scss';
+
+// pagination markup and styles
+
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+import * as TUI from './js/pagination';
+
+const pagination = new Pagination('pagination', TUI.getOptions(500));
+pagination.on('afterMove', event => {
+  console.log('knock knock');
+});
+
+// console.log(pagination);
