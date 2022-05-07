66d028ae7eeb044a6d466c94f6e69bbad680e145
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2022-05-07 18:21:28 +0300

    added modal and close/open btn

---
+++ src/js/movieModal.js
@@ -0,0 +1,28 @@
+const openModalRef = document.querySelector('.openModal');
+const backdropRef = document.querySelector('.js-backdrop');
+const btnCloseModalRef = document.querySelector('.js-modal__close-btn');
+
+openModalRef.addEventListener('click', onOpenMovieModal);
+btnCloseModalRef.addEventListener('click', onCloseMovieModal);
+backdropRef.addEventListener('click', onBackdropClick);
+function onOpenMovieModal(e) {
+  backdropRef.classList.add('show');
+  document.body.style.overflow = 'hidden';
+  document.addEventListener('keydown', onCloseByEsc);
+}
+
+function onCloseMovieModal(e) {
+  backdropRef.classList.remove('show');
+  document.body.style.overflow = 'scroll';
+}
+
+function onBackdropClick(e) {
+  if (e.target === backdropRef) return onCloseMovieModal();
+}
+
+function onCloseByEsc(e) {
+  if (e.code === 'Escape') {
+    onCloseMovieModal();
+  }
+}
+export { openModalRef };
